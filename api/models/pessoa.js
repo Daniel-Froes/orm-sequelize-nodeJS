70a142ee631c5c496dc4cf3aed1f19263c9aef43
Daniel Froes <danielfroestbf@gmail.com>
2022-07-08 20:49:07 -0300

    feat: testes sobre um CRUD e fake api

---
+++ api/models/pessoa.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pessoa extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Pessoa.hasMany(models.Turmas, {
+        foreignKey: 'docente_id'
+      })
+      Pessoa.hasMany(models.Matriculas, {
+        foreignKey: 'estudante_id',})
+    }
+  }
+  Pessoa.init({
+    nome: DataTypes.STRING,
+    ativo: DataTypes.BOOLEAN,
+    email: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Pessoa',
+  });
+  return Pessoa;
+};
